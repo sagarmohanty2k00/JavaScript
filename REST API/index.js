b9cd09034f67d11595741b801924c37120b53eee
sagarmohanty2k00 <sagarmohanty2k00@gmail.com>
2022-02-23 23:58:23 +0530

    added url path

--- REST API/index.js
+++ REST API/index.js
@@ -4,10 +4,22 @@
 */
 // imports
 const http = require('http')
+const url = require('url')
 
 // Creating a web server
 const server = http.createServer(function(req, res){
+    // Get the URL and pasrse it
+    const parsedUrl = url.parse(req.url, true); 
+
+    // Get the path
+    const path = parsedUrl.path;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // send the response
     res.end("Hello this is the starting point\n");
+
+    // log the path
+    console.log("reuqsted path: " + trimmedPath);
 });
 
 
